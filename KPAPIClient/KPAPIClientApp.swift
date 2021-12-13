d1ce4f4c884572a74e6f10d7cdca208e0b88b186
Paul Chan <paul.chan@rakuten.com>
2021-12-13 19:18:19 +0900

    Initial Commit

---
+++ KPAPIClient/KPAPIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPAPIClientApp.swift
+//  KPAPIClient
+//
+//  Created by Chan, Paul | Paul | RP on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct KPAPIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
